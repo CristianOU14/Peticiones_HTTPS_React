51ea6d7648edca48d29b22bae5264df750989339
CristianOU14 <cdocampo@correo.iue.edu.co>
2024-09-20 19:20:23 -0500

    Peticion

---
+++ Peticiones-React/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  //Almaceno la info de la api
+  const [data, setData] = useState([])
+  const fecthData = ()=>
+  {
+    const base = "https://api-colombia.com/api/v1/"
+    const enpoint = "Department"
+    const url = `${base}${enpoint}`
+    fetch(url)
+    .then(data => data.json())
+    .then(data => setData(data))
+    .then(error => console.log(error))
+  }
+  return (
+    <>
+     <h1>Peticiones HTTPS</h1>
+     <button onClick={()=>fecthData()}>Lamar API</button>
+     <div>
+        <ul>
+          {
+            data.map(item=>
+              {
+                return <li key={item.id}>{item.name}</li>
+              })
+          }
+        </ul>
+     </div>
+    </>
+  )
+}
+
+export default App
